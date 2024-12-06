5accb8164f452d6b3961e05db76c887fcd2f45c0
Mortens <mortens.4444@gmail.com>
2024-12-06 18:21:36 +0100

    Fix a namespace issue

---
+++ Mtf.LanguageService/Enums/ImplementedLanguage.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+
+namespace Mtf.LanguageService.Enums
+{
+    public enum ImplementedLanguage
+    {
+        [Description("العربية")]
+        Arabic,
+
+        [Description("中文")]
+        Chinese,
+
+        [Description("English")]
+        English,
+
+        [Description("Français")]
+        French,
+
+        [Description("ქართული")]
+        Georgian,
+
+        [Description("Deutch")]
+        German,
+
+        [Description("ελληνικά")]
+        Greek,
+
+        [Description("עברית")]
+        Hebrew,
+
+        [Description("हिन्दी")]
+        Hindi,
+
+        [Description("Magyar")]
+        Hungarian,
+
+        [Description("Italiano")]
+        Italian,
+
+        [Description("にほんご")]
+        Japanese,
+
+        [Description("한국어")]
+        Korean,
+
+        [Description("русский")]
+        Russian,
+
+        [Description("Español")]
+        Spanish
+    }
+}
