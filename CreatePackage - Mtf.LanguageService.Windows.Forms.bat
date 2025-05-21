eb9df9cecb809f8238895245f3da6c86fa00e167
Mortens <mortens.4444@gmail.com>
2025-05-21 22:31:21 +0200

    Remove binlog

--- CreatePackage - Mtf.LanguageService.Windows.Forms.bat
+++ CreatePackage - Mtf.LanguageService.Windows.Forms.bat
@@ -51,6 +51,6 @@ for /R %%P in (*.csproj) do (
 )
 
 powershell.exe -ExecutionPolicy Bypass -File ".\IncrementPackageVersion.ps1" -CsprojFile "%ProjectName%\%ProjectName%.csproj"
-dotnet pack --include-symbols --include-source %ProjectName%\%ProjectName%.csproj -c Release /p:IncludeSymbols=true /p:IncludeSource=true /p:DebugType=full /p:EmbedAllSources=true /p:Deterministic=true /bl
+dotnet pack --include-symbols --include-source %ProjectName%\%ProjectName%.csproj -c Release /p:IncludeSymbols=true /p:IncludeSource=true /p:DebugType=full /p:EmbedAllSources=true /p:Deterministic=true
 move .\%ProjectName%\bin\Release\*.nupkg %TargetDir%
 REM pause
